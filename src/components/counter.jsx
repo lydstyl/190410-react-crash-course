29a0f6ee3dfb1abbb5519e8da7f61520ff2e5c48
Brun <gabriel.brun@accenture.com>
2019-04-10 21:23:05 +0200

    increment

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,8 +1,40 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"]
+  };
+
   render() {
-    return <h1>Hello World</h1>;
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+      </div>
+    );
+  }
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += !this.state.count ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    console.log(this);
+
+    const { count } = this.state;
+    return !count ? "Zero" : count;
   }
 }
 
